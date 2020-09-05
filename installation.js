aba400205aa00ec0418d18236e7c016869bb7fc1
Matthias Giger <i@matthiasgiger.com>
2020-09-05 20:37:25 +0200

    refactor(skip): separate package for skipping local install

--- installation.js
+++ installation.js
@@ -2,15 +2,10 @@ import { join } from 'path'
 import { readFileSync, writeFileSync } from 'fs'
 import objectAssignDeep from 'object-assign-deep'
 import formatPackageJson from 'pakag'
+import skip from 'skip-local-postinstall'
 import configuration from './configuration/package.js'
 
-// Skip postinstall on local install.
-// https://stackoverflow.com/a/53239387/3185545
-const env = process.env
-if (env.INIT_CWD === env.PWD || env.INIT_CWD.indexOf(env.PWD) === 0) {
-  console.info('Skipping `postinstall` script on local installs')
-  process.exit(0)
-}
+skip()
 
 const packageJsonPath = join(process.cwd(), '../../package.json')
 
