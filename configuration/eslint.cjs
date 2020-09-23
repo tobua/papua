ac0d34c6a13363872f7eacc33d03f48c94effcec
Matthias Giger <i@matthiasgiger.com>
2020-09-23 23:25:17 +0200

    fix(eslint): add more lint rules and update dependencies

--- configuration/eslint.cjs
+++ configuration/eslint.cjs
@@ -12,12 +12,15 @@ const customRules = {
   'react/require-default-props': 0,
   // No line required between mobx observable properties.
   'lines-between-class-members': 0,
+  '@typescript-eslint/lines-between-class-members': 0,
   // Multiple mobx classes per file, especially for lists.
   'max-classes-per-file': 0,
   // Allow spreading props.
   'react/jsx-props-no-spreading': 0,
   // No need to write extensions.
   'import/extensions': 0,
+  // Often there is only the index available, would make plugin API more complex otherwise.
+  'react/no-array-index-key': 0,
 }
 
 const customSettings = {
