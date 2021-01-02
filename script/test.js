7a91063bac0166b46463ca191e001a7340b012ff
Matthias Giger <i@matthiasgiger.com>
2021-01-02 01:11:52 +0100

    feat(test): run Cypress with papua configuration if found

--- script/test.js
+++ script/test.js
@@ -1,8 +1,54 @@
 import { execSync } from 'child_process'
+import { realpathSync } from 'fs'
+import { join } from 'path'
+import isPathInside from 'is-path-inside'
+import globalDirs from 'global-dirs'
 import { log } from '../utility/helper.js'
+import { getProjectBasePath } from '../utility/path.js'
+
+const installCypressIfMissing = () => {
+  if (isPathInside('cypress', join(getProjectBasePath(), 'node_modules'))) {
+    return
+  }
+
+  // Adapted from is-installed-globally
+  if (isPathInside('cypress', realpathSync(globalDirs.npm.packages))) {
+    return
+  }
+
+  if (isPathInside('cypress', globalDirs.yarn.packages)) {
+    return
+  }
+
+  log('Cypress not found installing it now...')
+
+  execSync(`npm install cypress`, { stdio: 'inherit' })
+}
 
 export default () => {
+  const hasJest = isPathInside('test', getProjectBasePath())
+  const hasCypress = isPathInside('cypress', getProjectBasePath())
+
   const additionalArguments = process.argv.slice(3)
-  log('running tests..')
-  execSync(`jest ${additionalArguments.join(' ')}`, { stdio: 'inherit' })
+
+  if (hasJest) {
+    log('running tests with jest...')
+    execSync(`jest ${additionalArguments.join(' ')}`, { stdio: 'inherit' })
+  }
+
+  if (hasCypress) {
+    log('running cypress...')
+    installCypressIfMissing()
+    execSync(
+      `cypress open --config-file ./node_modules/papua/configuration/cypress.json`,
+      { stdio: 'inherit' }
+    )
+  }
+
+  if (!hasJest && !hasCypress) {
+    log(
+      'No tests found add tests inside /test for Jest or inside /cypress for Cypress',
+      'warning'
+    )
+  }
 }
